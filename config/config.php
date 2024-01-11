9d950ad6e4777e86818a9cbd688700e1cd9aee5e
Fleur N'guessan <ngfleur01@gmail.com>
2024-01-11 14:52:01 +0100

    Reset project

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Informations de connexion Ã  la base de donnÃ©es
-$host = "https://sql.phpnet.org/"; // L'hÃ´te de la base de donnÃ©es (gÃ©nÃ©ralement "localhost")
-$database = "sql11632"; // Le nom de la base de donnÃ©es que vous avez crÃ©Ã©e
-$username = "sql11632_2"; // Votre nom d'utilisateur MySQL
-$password = "5uxdOPcU6GPM"; // Votre mot de passe MySQL
-?>
-
-
